18d919feead7fc622a4941c111657b6966db10e7
rasgadocarlos2-jpg <rasgadocarlos2@gmail.com>
2026-01-14 21:39:56 +0000

    Refactor models and main application structure: replaced SQLAlchemy models with Pydantic models for Artist and Song, and updated main.py to include routers for artists and songs, removing direct database handling.

--- app/main.py
+++ app/main.py
@@ -1,34 +1,12 @@
 from fastapi import FastAPI
-from app.models import Artist, Song
+from app.routes import artists, songs
 
 app = FastAPI(title="Mukanda API")
 
-artists_db = []
-songs_db = []
+app.include_router(artists.router)
+app.include_router(songs.router)
+
 
 @app.get("/")
 def home():
     return {"mensagem": "Mukanda API está viva"}
-
-@app.post("/artists")
-def create_artist(artist: Artist):
-    artist.id = len(artists_db) + 1
-    artists_db.append(artist)
-    return artist
-
-@app.get("/artists")
-def list_artists():
-    return artists_db
-
-@app.post("/songs")
-def create_song(song: Song):
-    song.id = len(songs_db) + 1
-    songs_db.append(song)
-    return song
-
-@app.get("/songs")
-def list_songs():
-    return songs_db
-
-
-s
